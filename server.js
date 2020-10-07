e48fb79ecf418092008bd44188199efdb4f79d53
Marco <zaminerm@edu.htl-villach.at>
2020-10-07 08:09:38 +0200

    postman tests + User authorization

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 
+const { json } = require('express');
 /*************************************************************************************** */
 /*                            Chat App                                                   */
 /*                                                                                       */
@@ -10,29 +11,64 @@ const express = require('express')
 const app = express()
 const port = 2604
 const hostname = 'chatservice.informatik.htl-vil';
+const allowedUsers = [
+  {"username":"Andi", "password":"Russ"},
+  {"username":"test", "password":"test"},
+  { "username":"sokrates", "password":"dialog" },
+  { "username":"platon", "password":"idee" },
+  { "username":"aristoteles", "password":"stoffundform" },
+  { "username":"kant", "password":"aufklaerung" } 
+];
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-  let msg = `${formatDate()} ${req.method} ${req.url}`;
-  res.msg = msg;
+  try{
+    let credentials = JSON.parse(req.headers.authorization);
+    if(credentials.username && credentials.password && credentials.length != 2){
+      if(checkCredentials(credentials)){         
+        res.status(200);
+        res.statusMessage = "Hello " + credentials.username;  
+        next();
+      }
+      else{
+        res.status(401);
+        res.statusMessage = "Access denied";
+      }
+    }
+    else{
+      res.status(401);
+      res.statusMessage = "Access denied";
+    }
+    next();
+  } catch(ex){    
+    res.status(404).send('NOT FOUND');
+  }
+})
+
+app.use((req, res, next) => {
+  let msg = `${formatDate()} ${req.method} ${req.url} ${res.statusCode}-${res.statusMessage}`;
+  req.msg = msg;
   
   next(); 
 })
 
 app.use((req, res, next) => {
-  console.log(res.msg);
+  console.log(req.msg);
   next();
 })
 
+app.get('/whoiam', (req, res) => {
+  res.send(res.statusMessage);
+})
+
 app.get('/helloExpress', (req, res) => {
-  res.status(200).send('Hello World! Express up and running')
+  res.send('Hello World! Express up and running')
 })
 
 app.listen(port, () => {
-  console.log(`Chat app listening at http://${hostname}:${port}/helloExpress`)
+  console.log(`Chat app listening at http://${hostname}:${port}/`)
 })
-
 
 function formatDate(){
   var stunden, minuten, sekunden;
@@ -52,3 +88,12 @@ function formatDate(){
   zeit = stunden + minuten + sekunden + " Uhr";
   return zeit;
 }
+
+function checkCredentials(credentials){
+  if(allowedUsers.some(n => {
+    return (n.username == credentials.username && n.password == credentials.password);
+  }))
+    return true;
+  return false;
+}
+
